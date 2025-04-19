bcaf20e1ac41b5ef653d75a76491287f2563f4b2
SkaLe <39skale@gmail.com>
2025-04-19 13:09:33 +0300

    fixed old version macro

--- imgui.h
+++ imgui.h
@@ -77,13 +77,13 @@ Index of this file:
 
 // Version
 // (Integer encoded as XYYZZ for use in #if preprocessor conditionals. Work in progress versions typically starts at XYY99 then bounce up to XYY00, XYY01 etc. when release tagging happens)
-#define IMGUI_VERSION               "1.88"
-#define IMGUI_VERSION_NUM           18800
-#define IMGUI_CHECKVERSION()        ImGui::DebugCheckVersionAndDataLayout(IMGUI_VERSION, sizeof(ImGuiIO), sizeof(ImGuiStyle), sizeof(ImVec2), sizeof(ImVec4), sizeof(ImDrawVert), sizeof(ImDrawIdx))
-#define IMGUI_HAS_TABLE
-#define IMGUI_HAS_VIEWPORT          // Viewport WIP branch
-#define IMGUI_HAS_DOCK              // Docking WIP branch
-#define IMGUI_HAS_STACK_LAYOUT      1 // Stack-Layout PR #846
+// #define IMGUI_VERSION               "1.88"
+// #define IMGUI_VERSION_NUM           18800
+// #define IMGUI_CHECKVERSION()        ImGui::DebugCheckVersionAndDataLayout(IMGUI_VERSION, sizeof(ImGuiIO), sizeof(ImGuiStyle), sizeof(ImVec2), sizeof(ImVec4), sizeof(ImDrawVert), sizeof(ImDrawIdx))
+// #define IMGUI_HAS_TABLE
+// #define IMGUI_HAS_VIEWPORT          // Viewport WIP branch
+// #define IMGUI_HAS_DOCK              // Docking WIP branch
+// #define IMGUI_HAS_STACK_LAYOUT      1 // Stack-Layout PR #846
 
 // Define attributes of all API symbols declarations (e.g. for DLL under Windows)
 // IMGUI_API is used for core imgui functions, IMGUI_IMPL_API is used for the default backends files (imgui_impl_xxx.h)
